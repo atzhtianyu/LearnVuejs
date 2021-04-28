e6a6b5c23d2bcd6567e1325c50164484d2651ff0
zhtianyu <1420098499@qq.com>
2021-04-28 10:22:12 +0800

    webpack

---
+++ 13-webpack的使用/02-webpack的配置/webpack.config.js
@@ -0,0 +1,11 @@
+const path = module.require('path');
+
+module.exports = {
+  // 入口：可以是字符串，数组，对象，这里入口只有一个，所以写一个字符串即可
+  entry: './src/main.js',
+  // 出口：通常是一个对象，至少包含两个重要属性，path和filename
+  output: {
+    path: path.resolve(__dirname, 'dist'),  // 注意：path通常是一个绝对路径
+    filename: 'bundle.js'
+  }
+}
